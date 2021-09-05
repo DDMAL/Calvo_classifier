61f273cf1958593bd32039e3514d04858c8b7889
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2021-09-05 09:32:49 -0400

    turned the trainer into a library

--- fast_trainer_lib.py
+++ fast_trainer_lib.py
@@ -1,14 +1,6 @@
-"""Local Fast Trainer
-This is the file for running Calvo Fast Trainer loaclly. Make sure
-to have an 'Images' folder with the correct inputs in the same directory.
-If not, you can change the values in 'inputs' and 'outputs'.
-Simply run `python local_fast_trainer.py` to see the output.
-This will call `training_engine_sae.py`.
-It should generate 3 files in its current state. A background model,
-a Model 0, and a Log File.
-If you're running it in a Rodan container, this will be located in code/Rodan/rodan/jobs/Calvo_classifier
-If the container is already running, try `docker exec -it [container_name] bash` to run the script without
-stopping.
+"""Fast Trainer Library
+This file provides the code for model generation.
+Can be used in the standalone file or within Rodan.
 """
 
 import logging
@@ -19,96 +11,106 @@ import numpy as np
 import training_engine_sae as training
 import pdb
 
-batch_size = 1
-patch_height = 32
-patch_width = 256
-max_number_of_epochs = 1
-max_samples_per_class = 100
 
+class CalvoTrainer:
+    def __init__(
+        self,
+        batch_size,
+        patch_height,
+        patch_width,
+        max_number_of_epochs,
+        max_samples_per_class,
+        inputs,
+        outputs,
+    ):
+        self.batch_size = batch_size
+        self.patch_height = patch_height
+        self.patch_width = patch_width
+        self.max_number_of_epochs = max_number_of_epochs
+        self.max_samples_per_class = max_samples_per_class
+        self.inputs = inputs
+        self.outputs = outputs
 
-# Fail if arbitrary layers are not equal before training occurs.
-inputs = {
-    "Image": [{"resource_path": "Images/Halifax_Folio_42v.png"}],
-    "rgba PNG - Layer 0 (Background)": [
-        {"resource_path": "Images/042v_BackgroundForNeumes.png"}
-    ],
-    "rgba PNG - Layer 1": [{"resource_path": "Images/042v_Neumes.png"}],
-    "rgba PNG - Selected regions": [
-        {"resource_path": "Images/042v_SelectedRegion.png"}
-    ],
-}
-outputs = {
-    "Model 0": [{"resource_path": "Images/model0.hdf5"}],
-    "Model 1": [{"resource_path": "Images/model1.hdf5"}],
-    #"Log File": [{"resource_path": "Images/logfile"}],
-}
+    def runTrainer(self):
 
-input_ports = len([x for x in inputs if "Layer" in x])
-output_ports = len([x for x in outputs if "Model" in x or "Log file" in x])
-if input_ports not in [output_ports, output_ports - 1]: # So it still works if Log File is added as an output. 
-    raise Exception(
-        'The number of input layers "rgba PNG - Layers" does not match the number of'
-        ' output "Adjustable models"\n'
-        "input_ports: " + str(input_ports) + " output_ports: " + str(output_ports)
-    )
+        input_ports = len([x for x in self.inputs if "Layer" in x])
+        output_ports = len([x for x in self.outputs if "Model" in x or "Log file" in x])
+        if input_ports not in [
+            output_ports,
+            output_ports - 1,
+        ]:  # So it still works if Log File is added as an output.
+            raise Exception(
+                'The number of input layers "rgba PNG - Layers" does not match the number of'
+                ' output "Adjustable models"\n'
+                "input_ports: "
+                + str(input_ports)
+                + " output_ports: "
+                + str(output_ports)
+            )
 
-# Required input ports
-# TODO assert that all layers have the same number of inputs (otherwise it will crack afterwards)
-number_of_training_pages = len(inputs["Image"])
+        # Required input ports
+        # TODO assert that all layers have the same number of inputs (otherwise it will crack afterwards)
+        number_of_training_pages = len(self.inputs["Image"])
 
-input_images = []
-gts = []
+        input_images = []
+        gts = []
 
-# Create output models
-output_models_path = {}
+        # Create output models
+        output_models_path = {}
 
-for idx in range(number_of_training_pages):
-    input_image = cv2.imread(inputs["Image"][idx]["resource_path"], cv2.IMREAD_COLOR)  # 3-channel
-    background = cv2.imread(
-        inputs["rgba PNG - Layer 0 (Background)"][idx]["resource_path"],
-        cv2.IMREAD_UNCHANGED,
-    )  # 4-channel
-    regions = cv2.imread(
-        inputs["rgba PNG - Selected regions"][idx]["resource_path"],
-        cv2.IMREAD_UNCHANGED,
-    )  # 4-channel
+        for idx in range(number_of_training_pages):
+            input_image = cv2.imread(
+                self.inputs["Image"][idx]["resource_path"], cv2.IMREAD_COLOR
+            )  # 3-channel
+            background = cv2.imread(
+                self.inputs["rgba PNG - Layer 0 (Background)"][idx]["resource_path"],
+                cv2.IMREAD_UNCHANGED,
+            )  # 4-channel
+            regions = cv2.imread(
+                self.inputs["rgba PNG - Selected regions"][idx]["resource_path"],
+                cv2.IMREAD_UNCHANGED,
+            )  # 4-channel
 
-    # Create categorical ground-truth
-    gt = {}
-    TRANSPARENCY = 3
-    regions_mask = regions[:, :, TRANSPARENCY] == 255
-    # background is already restricted to the selected regions (based on Pixel.js' behaviour)
+            # Create categorical ground-truth
+            gt = {}
+            TRANSPARENCY = 3
+            regions_mask = regions[:, :, TRANSPARENCY] == 255
+            # background is already restricted to the selected regions (based on Pixel.js' behaviour)
 
-    # Populate remaining inputs and outputs
-    bg_mask = background[:, :, TRANSPARENCY] == 255
-    gt["0"] = np.logical_and(bg_mask, regions_mask)
+            # Populate remaining inputs and outputs
+            bg_mask = background[:, :, TRANSPARENCY] == 255
+            gt["0"] = np.logical_and(bg_mask, regions_mask)
 
-    for i in range(1, input_ports):
-        file_obj = cv2.imread(
-            inputs["rgba PNG - Layer {layer_num}".format(layer_num=i)][idx]["resource_path"],
-            cv2.IMREAD_UNCHANGED,
+            for i in range(1, input_ports):
+                file_obj = cv2.imread(
+                    inputs["rgba PNG - Layer {layer_num}".format(layer_num=i)][idx][
+                        "resource_path"
+                    ],
+                    cv2.IMREAD_UNCHANGED,
+                )
+                file_mask = file_obj[:, :, TRANSPARENCY] == 255
+                gt[str(i)] = np.logical_and(file_mask, regions_mask)
+
+            input_images.append(input_image)
+            gts.append(gt)
+
+        for i in range(input_ports):
+            output_models_path[str(i)] = self.outputs["Model " + str(i)][0][
+                "resource_path"
+            ]
+            # THIS IS NOT TAKING INTO ACCOUNT ANY FILE NOT NAMED MODEL IE BACKGROUND AND LOG!!!!
+
+        # Call in training function
+        status = training.train_msae(
+            input_images=input_images,
+            gts=gts,
+            num_labels=input_ports,
+            height=self.patch_height,
+            width=self.patch_width,
+            output_path=output_models_path,
+            epochs=self.max_number_of_epochs,
+            max_samples_per_class=self.max_samples_per_class,
+            batch_size=self.batch_size,
         )
-        file_mask = file_obj[:, :, TRANSPARENCY] == 255
-        gt[str(i)] = np.logical_and(file_mask, regions_mask)
 
-    input_images.append(input_image)
-    gts.append(gt)
-
-for i in range(input_ports):
-    output_models_path[str(i)] = outputs["Model " + str(i)][0]["resource_path"]
-    # THIS IS NOT TAKING INTO ACCOUNT ANY FILE NOT NAMED MODEL IE BACKGROUND AND LOG!!!!
-
-# Call in training function
-status = training.train_msae(
-    input_images=input_images,
-    gts=gts,
-    num_labels=input_ports,
-    height=patch_height,
-    width=patch_width,
-    output_path=output_models_path,
-    epochs=max_number_of_epochs,
-    max_samples_per_class=max_samples_per_class,
-    batch_size=batch_size,
-)
-
-print("Finishing the Fast CM trainer job.")
+        print("Finishing the Fast CM trainer job.")
